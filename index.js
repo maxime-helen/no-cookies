5589dbdfc34e847fe596d041fbfb16643ecaa9be
maxime-helen <helenmaxime@gmail.com>
2020-08-17 00:53:20 -0700

    Touch keywords and use of airbnb babel guideline

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-'use strict'
-
-let cookieDescriptor
+let cookieDescriptor;
 
 const NoCookies = {
   disable: (whitelist = []) => {
-    const documentPrototype = Object.getPrototypeOf(document)
-    cookieDescriptor = Object.getOwnPropertyDescriptor(documentPrototype, 'cookie')
+    const documentPrototype = Object.getPrototypeOf(document);
+    cookieDescriptor = Object.getOwnPropertyDescriptor(documentPrototype, 'cookie');
     Object.defineProperty(document, 'cookie', {
       get: () => {
-        let newCookies = ''
-        whitelist.forEach(item => {
+        let newCookies = '';
+        whitelist.forEach((item) => {
           const cookieValue = documentPrototype
             .cookie
             .split('; ')
-            .find(cookie => cookie.startsWith(item))
+            .find((cookie) => cookie.startsWith(item));
           if (cookieValue) {
-            newCookies += `${cookieValue}; `
+            newCookies += `${cookieValue}; `;
           }
-        })
-        return newCookies
+        });
+        return newCookies;
       },
       set: (cookie) => {
-        if (whitelist.some(item => cookie.startsWith(item))) {
-          documentPrototype.cookie = cookie
+        if (whitelist.some((item) => cookie.startsWith(item))) {
+          documentPrototype.cookie = cookie;
         }
       },
-      configurable: true
-    })
+      configurable: true,
+    });
   },
   enable: () => {
     if (cookieDescriptor) {
-      Object.defineProperty(document, 'cookie', cookieDescriptor)
-      cookieDescriptor = undefined
+      Object.defineProperty(document, 'cookie', cookieDescriptor);
+      cookieDescriptor = undefined;
     }
-  }
-}
+  },
+};
 
-export default NoCookies
+export default NoCookies;
--- test/unit.js
+++ test/unit.js
@@ -1,72 +1,70 @@
-'use strict'
+import assert from 'assert';
+import NoCookies from '../index';
 
-import assert from 'assert'
-import NoCookies from '../index.js'
+let cookieTestValue = '';
 
-let cookieTestValue = ''
-
-describe('Unit test suit', function () {
-  beforeEach('Mock document.cookie', function () {
-    let mockedDocument = {}
+describe('Unit test suit', () => {
+  beforeEach('Mock document.cookie', () => {
+    let mockedDocument = {};
     Object.defineProperty(mockedDocument, 'cookie', {
       get: () => cookieTestValue,
       set: (newCookies) => {
-        cookieTestValue = newCookies
+        cookieTestValue = newCookies;
       },
-      configurable: true
-    })
-    mockedDocument = Object.create(mockedDocument)
-    global.document = mockedDocument
-  })
+      configurable: true,
+    });
+    mockedDocument = Object.create(mockedDocument);
+    global.document = mockedDocument;
+  });
 
-  afterEach('Cleanup document', function () {
-    global.document = undefined
-    cookieTestValue = ''
-  })
+  afterEach('Cleanup document', () => {
+    global.document = undefined;
+    cookieTestValue = '';
+  });
 
-  it('disable() prevents accessors to cookie', function () {
-    cookieTestValue = 'foo=1; '
-    assert(document.cookie === cookieTestValue)
-    NoCookies.disable()
-    assert(document.cookie === '')
-    document.cookie = 'bar=1; '
-    assert(document.cookie === '')
-  })
+  it('disable() prevents accessors to cookie', () => {
+    cookieTestValue = 'foo=1; ';
+    assert(document.cookie === cookieTestValue);
+    NoCookies.disable();
+    assert(document.cookie === '');
+    document.cookie = 'bar=1; ';
+    assert(document.cookie === '');
+  });
 
-  it('disable() with whitelisted cookie allows accessors', function () {
-    cookieTestValue = 'foo=1; bar=2; '
-    assert(document.cookie === cookieTestValue)
-    NoCookies.disable(['foo'])
-    assert(document.cookie === 'foo=1; ')
-    document.cookie = 'foo=2; '
-    assert(document.cookie === 'foo=2; ')
-  })
+  it('disable() with whitelisted cookie allows accessors', () => {
+    cookieTestValue = 'foo=1; bar=2; ';
+    assert(document.cookie === cookieTestValue);
+    NoCookies.disable(['foo']);
+    assert(document.cookie === 'foo=1; ');
+    document.cookie = 'foo=2; ';
+    assert(document.cookie === 'foo=2; ');
+  });
 
-  it('disable() and set whitelisted cookie', function () {
-    cookieTestValue = 'foo=1; '
-    assert(document.cookie === cookieTestValue)
-    NoCookies.disable(['bar'])
-    assert(document.cookie === '')
-    document.cookie = 'bar=2; '
-    assert(document.cookie === 'bar=2; ')
-  })
+  it('disable() and set whitelisted cookie', () => {
+    cookieTestValue = 'foo=1; ';
+    assert(document.cookie === cookieTestValue);
+    NoCookies.disable(['bar']);
+    assert(document.cookie === '');
+    document.cookie = 'bar=2; ';
+    assert(document.cookie === 'bar=2; ');
+  });
 
-  it('enable() without prior disable() does nothing', function () {
-    cookieTestValue = 'foo=1; '
-    NoCookies.enable()
-    assert(document.cookie === cookieTestValue)
-    document.cookie = 'foo=2; '
-    assert(document.cookie === 'foo=2; ')
-  })
+  it('enable() without prior disable() does nothing', () => {
+    cookieTestValue = 'foo=1; ';
+    NoCookies.enable();
+    assert(document.cookie === cookieTestValue);
+    document.cookie = 'foo=2; ';
+    assert(document.cookie === 'foo=2; ');
+  });
 
-  it('enable() after prior disable() sets back native behavior', function () {
-    cookieTestValue = 'foo=1; '
-    assert(document.cookie === cookieTestValue)
-    NoCookies.disable()
-    assert(document.cookie === '')
-    NoCookies.enable()
-    assert(document.cookie === cookieTestValue)
-    document.cookie = 'foo=2; '
-    assert(document.cookie === 'foo=2; ')
-  })
-})
+  it('enable() after prior disable() sets back native behavior', () => {
+    cookieTestValue = 'foo=1; ';
+    assert(document.cookie === cookieTestValue);
+    NoCookies.disable();
+    assert(document.cookie === '');
+    NoCookies.enable();
+    assert(document.cookie === cookieTestValue);
+    document.cookie = 'foo=2; ';
+    assert(document.cookie === 'foo=2; ');
+  });
+});
